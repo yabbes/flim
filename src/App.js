b1ecb1aca68ec58b05f95e10c8e0d802c611b835
Tobias Machulski <yabbaspost@gmail.com>
2017-08-02 11:04:30 +0200

    search is working (more or less)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import $ from 'jquery';
 import axios from 'axios';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      query: 'hal'
+      query: ''
     };
   }
   handleChange(event){
@@ -21,7 +20,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Flim-base</h2>
+          <h2>Welcome to Flim-base with React</h2>
         </div>
         <p className="App-intro">
           <SearchBar handleChange={(event) => this.handleChange(event)}/>
@@ -45,7 +44,6 @@ class FilmList extends Component {
   }
 
   getMovieList(name){
-    var results = [];
     axios.get('https://api.themoviedb.org/3/discover/movie?api_key=a8e2ba7b760d6d3d601e43eb696df1ac')
     .then(function (response) {
       //console.log(response);
@@ -58,12 +56,39 @@ class FilmList extends Component {
     });
     
   }
+  searchFor(movie){
+    axios.get('https://api.themoviedb.org/3/search/movie?query=' + movie + '&api_key=a8e2ba7b760d6d3d601e43eb696df1ac')
+    .then(function (response) {
+      this.setState({
+        items: response.data.results
+      })
+    }.bind(this))
+    .catch(function (error) {
+      console.log(error);
+    });
 
+  }
+  componentWillReceiveProps(){
+    //console.log(this);
+    //console.log(this.props);
+    if (this.props.query !== ''){
+      this.setState({
+        initialList: false,
+        query: this.props.query
+      });
+      let movie = this.state.query;
+      //console.log('the movie is:' + movie);
+      this.searchFor(movie);
+
+    }
+  }
   componentDidMount() {
+    //console.log(this.props);
     if (this.state.initialList) {
       this.getMovieList();
     }
-    console.log(this);
+    
+    
     
 
   }
